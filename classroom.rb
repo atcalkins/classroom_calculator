743aaa70e7800b2a297bc08c27378a36a9cbd31e
Tyler Calkins <atcalkins@outlook.com>
2017-08-22 15:04:53 -0400

    passed first two tests

--- classroom.rb
+++ classroom.rb
@@ -2,24 +2,30 @@
 # for that student and assignment. Note that Ruby counts arrays from 0, but
 # we are referring to them as 1-10.
 def assignment_score(grade_hash, student, assignment_num)
+  grade_hash[student][assignment_num - 1]
 end
 
 # Given a grade_hash and assignment number, return all scores for that
 # assignment. Note that Ruby counts arrays from 0, but we are referring to
 # them as 1-10.
 def assignment_scores(grade_hash, assignment_num)
+  grade_hash.map do | key, value |
+    value[assignment_num - 1]
+  end
 end
 
 # Given a grade_hash and assignment number, return the average score for that
 # assignment. Note that Ruby counts arrays from 0, but we are referring to
 # them as 1-10.
 def assignment_average_score(grade_hash, assignment_num)
+
 end
 
 # Return a hash of students and their average score.
 # TIP: To convert an array like [[:indiana, 90], [:nevada, 80]] to a hash,
 # use .to_h. Also look at Hash#transform_values.
 def averages(grade_hash)
+
 end
 
 # Return a letter grade for a numerical score.
@@ -29,6 +35,7 @@ end
 # 60-69 => D
 # < 60 => F
 def letter_grade(score)
+
 end
 
 # Return a hash of students and their final letter grade, as determined
